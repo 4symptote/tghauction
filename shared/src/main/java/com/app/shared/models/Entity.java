57b93567ab6f33f927dbf4bd8a106b7b8fbee687
s1698 <25021698@vnu.edu.vn>
2026-04-06 13:14:13 +0700

    ABCDEdwad

--- shared/src/main/java/com/app/shared/models/Entity.java
+++ shared/src/main/java/com/app/shared/models/Entity.java
@@ -13,6 +13,6 @@ public abstract class Entity implements Serializable {
     public void setId(String id) { this.id = id; }
     public String getId() { return id; }
 
-    public void test() {}
+    public void test() {} // adwda
 
 }
